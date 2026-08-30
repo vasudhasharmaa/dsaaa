94eae916cfe46353c5baa4c915fa7c6148613010
Vasudha Sharma <vasudhasharmag@gmail.com>
2026-08-30 19:41:04 +0530

    Time: 0 ms (100.00%) | Memory: 11.8 MB (55.46%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -7,7 +7,7 @@ int f(vector<vector<int>>& obstacleGrid,int r,int c,vector<vector<int>>&dp)
     if(obstacleGrid[m][n]==1) return 0;
     if(m == 0 && n == 0 && obstacleGrid[0][0] == 0)
         return 1;
-    // if(m == 0 && n == 0 && obstacleGrid[0][0] == 1) return 0;
+    
 
     if(dp[m][n]!=-1)return dp[m][n];
     dp[m][n]=0;
