aab414089cfef3ad715569c528bfba0f6a408254
Vasudha Sharma <vasudhasharmag@gmail.com>
2026-08-30 21:31:18 +0530

    Time: 0 ms (100.00%) | Memory: 11.7 MB (72.24%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-int f(vector<vector<int>>& obstacleGrid,int r,int c,vector<vector<int>>&dp)
+/*int f(vector<vector<int>>& obstacleGrid,int r,int c,vector<vector<int>>&dp)
 {
     int m=r;
     int n=c;
@@ -28,12 +28,37 @@ int f(vector<vector<int>>& obstacleGrid,int r,int c,vector<vector<int>>&dp)
     
     return dp[m][n];
 
-}
+}*/
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
         int m=obstacleGrid.size();
         int n=obstacleGrid[0].size();
-        vector<vector<int>>dp(m,vector<int>(n,-1));
+        vector<vector<int>>dp(m,vector<int>(n,0));
 
-        return f(obstacleGrid,m-1,n-1,dp);
+        if(obstacleGrid[0][0]==1)return 0;
+        dp[0][0] = 1;
+        
+        for(int i=1;i<n;i++)
+        {
+            if(obstacleGrid[0][i]==0)dp[0][i]=dp[0][i-1];
+        }
+        for(int i=1;i<m;i++)
+        {
+            if(obstacleGrid[i][0]==0)dp[i][0]=dp[i-1][0];
+        }
+     
+
+        for(int i=1;i<m;i++)
+        {
+            for(int j=1;j<n;j++)
+            {
+                if(obstacleGrid[i][j]==0)
+                {
+                    dp[i][j]=dp[i][j-1]+dp[i-1][j];
+                }
+                else dp[i][j]=0;
+            }
+        }
+        return dp[m-1][n-1];
+
     }
 };
