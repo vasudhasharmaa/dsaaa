3543c3f16ce88ad740460da1f126ac8337425577
Vasudha Sharma <vasudhasharmag@gmail.com>
2026-09-03 10:35:16 +0530

    Time: 0 ms (100.00%) | Memory: 13.5 MB (25.67%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -1,30 +1,43 @@
 class Solution {
 public:
-int f(int i,int j,vector<vector<int>>&dp,vector<vector<int>>&triangle)
-{
-    int m=triangle.size();
-    int sum=0;
-    if(dp[i][j]!=INT_MAX)
+    /*int f(int i,int j,vector<vector<int>>&dp,vector<vector<int>>&triangle)
     {
-        return dp[i][j];
-    }
-    if(i==m-1)
-    {
-        return dp[i][j]=triangle[i][j];
-    }
-   
+        int m=triangle.size();
+        int sum=0;
+        if(dp[i][j]!=INT_MAX)
+        {
+            return dp[i][j];
+        }
+        if(i==m-1)
+        {
+            return dp[i][j]=triangle[i][j];
+        }
 
-    
-    int down=triangle[i][j]+f(i+1,j,dp,triangle);
-    int dia=triangle[i][j]+f(i+1,j+1,dp,triangle);
-    int choose=min(down,dia);
-    sum+=choose;
-    return dp[i][j]=sum;
 
-}
+
+        int down=triangle[i][j]+f(i+1,j,dp,triangle);
+        int dia=triangle[i][j]+f(i+1,j+1,dp,triangle);
+        int choose=min(down,dia);
+        sum+=choose;
+        return dp[i][j]=sum;
+
+    }*/
     int minimumTotal(vector<vector<int>>& triangle) {
-       int m=triangle.size();
-       vector<vector<int>>dp(m,vector<int>(m,INT_MAX));
-    return f(0,0,dp,triangle);
+        int m = triangle.size();
+        vector<vector<int>> dp(m, vector<int>(m, INT_MAX));
+        
+        for (int j = 0; j < m; j++) {
+            dp[m - 1][j] = triangle[m - 1][j];
+        }
+        for (int i = m - 2; i >= 0; i--) {
+
+            for (int j = 0; j <= i; j++) {
+                int down = triangle[i][j] + dp[i + 1][j];
+                int dia = triangle[i][j] + dp[i + 1][j + 1];
+                int choose = min(down, dia);
+                dp[i][j]=choose;
+            }
+        }
+        return dp[0][0];
     }
 };
