1be0f40320b849faf91f2a58bf221f0b8d576e70
Vasudha Sharma <vasudhasharmag@gmail.com>
2026-09-03 19:49:45 +0530

    Time: 0 ms (100.00%) | Memory: 23.4 MB (20.53%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.cpp
+++ 110-balanced-binary-tree/balanced-binary-tree.cpp
@@ -6,42 +6,46 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
-    int maxDepth(TreeNode* root) {
-        if (root == NULL)
-            return 0;
-        int maxL = maxDepth(root->left);
-        int maxR = maxDepth(root->right);
-
-        return 1 + max(maxL, maxR);
-    }
-
 public:
+int maxDepth(TreeNode*root)
+{
+    if(root==NULL)return 0;
+    int maxL=maxDepth(root->left);
+    int maxR=maxDepth(root->right);
+    return 1+max(maxL,maxR);
+}
+
     bool isBalanced(TreeNode* root) {
-        if (root == NULL)
-            return true;
-        stack<TreeNode*> st;
-        st.push(root);
+        
+        queue<TreeNode*>q;
+        q.push(root);
+        if(root==NULL)return true;
+        while(!q.empty())
+        {
+            TreeNode* node=q.front();
 
-        while (!st.empty()) {
-            TreeNode* node = st.top();
-            st.pop();
-            int right = maxDepth(node->right);
-            int left = maxDepth(node->left);
-            if ((abs(right - left) > 1))
-                return false;
-
-            if (node->right)
-                st.push(node->right);
-            if (node->left)
-                st.push(node->left);
+            q.pop();
+          
+            if(abs(maxDepth(node->left)-maxDepth(node->right))<=1)
+            {
+                if(node->left!=NULL)q.push(node->left);
+                if(node->right!=NULL)q.push(node->right);
+                
+            }
+            else
             
+            {
+                return false;
+            }
+
+
         }
-        if(st.empty())return true;
-        return false;
+        return true;
+
+        
     }
 };
